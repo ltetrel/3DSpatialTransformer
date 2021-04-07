77171cee7b4c01d29a8ae72913b434767317292a
ltetrel <loic.tetrel.pro@gmail.com>
2021-04-07 18:26:22 -0400

    replaced tf condition by if; re-worked transofrmation matrix computation; improving tests

--- spatial_transformer_3D.py
+++ spatial_transformer_3D.py
@@ -1,4 +1,4 @@
-"""Implementation of the Spatial Transformer in 3D.
+"""Implementation of the Spatial Transformer Layer, for 3D quaternions.
 
 @misc{jaderberg2016spatial,
       title={Spatial Transformer Networks}, 
@@ -15,37 +15,52 @@ NOTE: Nearest neighbor interpolation is not available during training, because o
 import tensorflow as tf
 
 def repeat(x, num_reps):
+    """ Repeat input multiple times """
     num_reps = tf.cast(num_reps, dtype=tf.int32)
-    x = tf.expand_dims(x, axis=1)
-    return tf.tile(x, multiples=(1,num_reps))
+    if tf.rank(x) == 1:
+        x = tf.expand_dims(x, axis=1)
+    return tf.tile(x, multiples=(1, num_reps))
 
-def quat_to_3D_matrix(q):
-    size_q = tf.shape(q[...,0])
-    R = tf.stack([1 - 2.*(q[...,2]**2 + q[...,3]**2), 2*(q[...,1]*q[...,2] - q[...,0]*q[...,3]), 2*(q[...,0]*q[...,2] + q[...,1]*q[...,3]), tf.zeros(size_q),
-                    2.*(q[...,1]*q[...,2] + q[...,0]*q[...,3]), 1 - 2.*(q[...,1]**2 + q[...,3]**2), 2.*(q[...,2]*q[...,3] - q[...,0]*q[...,1]), tf.zeros(size_q),
-                    2.*(q[...,1]*q[...,3] - q[...,0]*q[...,2]), 2.*(q[...,0]*q[...,1] + q[...,2]*q[...,3]), 1 - 2.*(q[...,1]**2 + q[...,2]**2), tf.zeros(size_q),
-                    tf.zeros(size_q), tf.zeros(size_q), tf.zeros(size_q), tf.ones(size_q)],axis=-1)
+def from_quaternion(quaternion):
+    """ Return the 3D rotation matrix from quaternion [a, b, c, w]. """
+    x, y, z, w = tf.unstack(quaternion, axis=-1)
+    tx = 2.0 * x
+    ty = 2.0 * y
+    tz = 2.0 * z
+    twx = tx * w
+    twy = ty * w
+    twz = tz * w
+    txx = tx * x
+    txy = ty * x
+    txz = tz * x
+    tyy = ty * y
+    tyz = tz * y
+    tzz = tz * z
+    R = tf.stack((1.0 - (tyy + tzz), txy - twz, txz + twy,
+                  txy + twz, 1.0 - (txx + tzz), tyz - twx,
+                  txz - twy, tyz + twx, 1.0 - (txx + tyy)),
+                 axis=-1)
 
-    return tf.reshape(R, (-1, 4, 4))
+    return tf.reshape(R, shape=(-1, 3, 3))
 
-def get_matrix_from_params(transfos, num_elems):
+def matrix_from_params(transfos):
+    """ Get the augmented transformation matrix from quaternion, translation and scaling parameters. """
     num_batch = tf.shape(transfos)[0]
-    scaling = tf.shape(transfos)[-1] == 10
-    trans = tf.shape(transfos)[-1] == 7
 
-    #if the transformations is [q0, q1, q2, q3, tx, ty, tz, sx, sy, sz], then we apply scaling
-    def apply_scaling(): return tf.linalg.diag(1./transfos[:, -3:])
-    def default_scaling(): return tf.eye(num_rows=3, batch_shape=[num_batch])
-    thetas = tf.cond(scaling, apply_scaling, default_scaling)
-
-    #if the transformations is [q0, q1, q2, q3, tx, ty, tz], then we apply translation
-    def apply_trans(): return tf.concat(axis=2, values=[thetas, transfos[:, 4:7, tf.newaxis]])
-    def default_trans(): return tf.concat(axis=2, values=[thetas, tf.zeros((num_batch, 3, 1))])
-    thetas = tf.cond(trans, apply_trans, default_trans)
-
-    #transformations should always be at least [q0, q1, q2, q3]
-    R = quat_to_3D_matrix(transfos[:, :4])
-    thetas = tf.linalg.matmul(thetas, R)
+    # scaling [q0, q1, q2, q3, tx, ty, tz, sx, sy, sz]
+    if tf.shape(transfos)[-1] == 10:
+        scaling = tf.linalg.diag(transfos[:, -3:])
+    else:
+        scaling = tf.eye(num_rows=3, batch_shape=(num_batch,))
+    # rotation [q0, q1, q2, q3]
+    rotation = from_quaternion(transfos[..., :4])
+    # theta = S @ R
+    scale_rotation = tf.linalg.matmul(scaling, rotation)
+    # translation [q0, q1, q2, q3, tx, ty, tz]
+    if tf.shape(transfos)[-1] == 7:
+        thetas = tf.concat([scale_rotation, transfos[:, 4:7, tf.newaxis]], axis=2)
+    else:
+        thetas = tf.concat([scale_rotation, tf.zeros((num_batch, 3, 1))], axis=2)
 
     return thetas
 
@@ -75,18 +90,38 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
         self.interp_method = tf.constant(interp_method, dtype=tf.string)
         self.padding_mode = tf.constant(padding_mode, dtype=tf.string)
 
-        if tf.math.logical_and(self.trainable == tf.constant(True), self.interp_method == tf.constant("nn")):
-            raise Exception("Cannot train with nearest-neighbor interpolator because it is not derivable!") 
-
     def build(self, input_shape):
-        super(self.__class__, self).build(input_shape)
         num_dims = input_shape[0].ndims - 2
         shape_grid = tf.shape(self.min_ref_grid)[0]
 
-        def ref_grid():
-            self.min_ref_grid = (-1) * tf.ones(num_dims, dtype=tf.float32)
-            self.max_ref_grid = tf.ones(num_dims, dtype=tf.float32)
-        tf.cond(num_dims != shape_grid, ref_grid, lambda *args: None)
+        # inputs is a list of size 2
+        tf.debugging.assert_equal(len(input_shape), 2)
+        # transformation size is at least 4 (quaternion), but no more than 10 (quaternion + translation + scale)
+        tf.debugging.assert_greater_equal(input_shape[1][-1], 4)
+        tf.debugging.assert_less_equal(input_shape[1][-1], 10)
+        # interpolation method
+        check_interpolation = tf.reduce_any(tf.math.equal(self.interp_method, tf.constant(["bilinear", "nn"])))
+        tf.debugging.assert_equal(check_interpolation, True
+            , message='{} must be anything between {}, but is {}'.format(
+                "interp_method", "[\"bilinear\", \"nn\"]", self.interp_method))
+        # padding method
+        check_padding = tf.reduce_any(tf.math.equal(self.padding_mode, tf.constant(["border", "zeros", "min"])))
+        tf.debugging.assert_equal(check_padding, True
+            , message='{} must be anything between {}, but is {}'.format(
+                "padding_mode", "[\"border\", \"zeros\", \"min\"]", self.padding_mode))
+
+        # validate reference grid values
+        if tf.math.not_equal(num_dims, shape_grid):
+            if tf.math.equal(shape_grid, 1):
+                self.min_ref_grid = self.min_ref_grid[0] * tf.ones(num_dims, dtype=tf.float32)
+                self.max_ref_grid = self.max_ref_grid[0] * tf.ones(num_dims, dtype=tf.float32)
+            else:
+                self.min_ref_grid = (-1) * tf.ones(num_dims, dtype=tf.float32)
+                self.max_ref_grid = tf.ones(num_dims, dtype=tf.float32)
+        # cannot train if method is nearest neighbor
+        if tf.math.logical_and(self.trainable == tf.constant(True), self.interp_method == tf.constant("nn")):
+            raise Exception("Cannot train with nearest-neighbor interpolator because it is not derivable!")
+        super(self.__class__, self).build(input_shape) 
 
     def compute_output_shape(self, input_shape):
         return input_shape[0]
@@ -100,31 +135,39 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
         }
 
     def call(self, inputs):
+        """Call a 3D Spatial Transformer layer.
+        
+        Args:
+            inputs: a `list` of two Tensor with shape `[A0, W, H, D, C]` and `[A0, N]`.
+                The first element of this list is the input 3D volume to be resampled,
+                the second element is the normalized quaternion to apply with optionnal
+                translations and scaling `[a, b, c, w, tx, ty, tz, sx, sy, sz]`, where
+                the quaternion Q = a*i + b*j + c*k + w.
+            training: flag to control batch normalization update statistics.
+
+        Returns:
+            Tensor with shape `[A0, W, H, D, C]`.
+        """
         img, transfos = inputs
         output = self._resample(img, transfos)
         return output
 
     def _resample(self, img, transfos):
-        """Constructs a 3D Spatial Transformer layer.
-        
-        Args:
-            inputs: a dense tensor of size `[B, N, 1, D]`.
-            training: flag to control batch normalization update statistics.
 
-        Returns:
-            Tensor with shape `[B, N, 1, C]`.
-        """
         input_shape = tf.shape(img)
         ref_size = input_shape[1:-1]
         ref_size_xyz = tf.concat([ref_size[1::-1], ref_size[2:]], axis=0)
 
-        input_transformed = self._transform_grid(ref_size_xyz, transfos=transfos, min_ref_grid=self.min_ref_grid, max_ref_grid=self.max_ref_grid)
+        input_transformed = self._transform_grid(ref_size_xyz
+                                                , transfos=transfos
+                                                , min_ref_grid=self.min_ref_grid
+                                                , max_ref_grid=self.max_ref_grid)
         input_transformed = self._interpolate(im=img
-                                            , points=input_transformed
-                                            , min_ref_grid=self.min_ref_grid
-                                            , max_ref_grid=self.max_ref_grid
-                                            , method=self.interp_method
-                                            , padding_mode=self.padding_mode)
+                                             , points=input_transformed
+                                             , min_ref_grid=self.min_ref_grid
+                                             , max_ref_grid=self.max_ref_grid
+                                             , method=self.interp_method
+                                             , padding_mode=self.padding_mode)
         output = tf.reshape(input_transformed, shape=input_shape)
 
         return output
@@ -132,7 +175,7 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
     def _transform_grid(self, ref_size_xyz, transfos, min_ref_grid, max_ref_grid):
         num_batch = tf.shape(transfos)[0]
         num_elems = tf.reduce_prod(ref_size_xyz)
-        thetas = get_matrix_from_params(transfos, num_elems)
+        thetas = matrix_from_params(transfos)
 
         # grid creation from volume affine
         mz, my, mx = tf.meshgrid(tf.linspace(min_ref_grid[2], max_ref_grid[2], ref_size_xyz[2])
@@ -151,7 +194,8 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
     
     def _interpolate(self, im, points, min_ref_grid, max_ref_grid, method="bilinear", padding_mode="zeros"):
         num_batch = tf.shape(im)[0]
-        vol_shape_xyz = tf.cast(tf.concat([tf.shape(im)[1:-1][1::-1], tf.shape(im)[1:-1][2:]], axis=0), dtype=tf.float32)
+        vol_shape_xyz = tf.concat([tf.shape(im)[1:-1][1::-1], tf.shape(im)[1:-1][2:]], axis=0)
+        vol_shape_xyz = tf.cast(vol_shape_xyz, dtype=tf.float32)
         width = vol_shape_xyz[0]
         height = vol_shape_xyz[1]
         depth = vol_shape_xyz[2]
@@ -164,6 +208,8 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
         zero = tf.zeros([], dtype=tf.float32)
         zero_i = tf.zeros([], dtype=tf.int32)
         ibatch = repeat(num_row_major[-1] * tf.range(num_batch, dtype=tf.int32), num_row_major[-1])
+        output = tf.zeros(shape_output, dtype=tf.float32)
+        valid = tf.ones([])
 
         # scale positions to [0, width/height - 1]
         coeff_x = (width - 1.)/(max_ref_grid[0] - min_ref_grid[0])
@@ -173,20 +219,16 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
         iy = (coeff_y * points[:, 1, :]) - (coeff_y *  min_ref_grid[1])
         iz = (coeff_z * points[:, 2, :]) - (coeff_z *  min_ref_grid[2])
 
-        # zeros padding mode, for positions outside of refrence grid
-        cond = tf.math.logical_or(tf.math.equal(padding_mode, tf.constant("zeros", dtype=tf.string))
-                                  , tf.math.equal(padding_mode, tf.constant("min", dtype=tf.string)))
-        def evaluate_valid(): return tf.expand_dims(tf.cast(tf.less_equal(ix, width - 1.) & tf.greater_equal(ix, zero)
-                                             & tf.less_equal(iy, height - 1.) & tf.greater_equal(iy, zero)
-                                             & tf.less_equal(iz, depth - 1.) & tf.greater_equal(iz, zero)
-                                             , dtype=tf.float32), -1)
-        def default(): return tf.ones([], dtype=tf.float32)
-        valid = tf.cond(cond, evaluate_valid, default)
+        # zeros and min padding mode, for positions outside of refrence grid
+        if tf.math.logical_or(tf.math.equal(padding_mode, tf.constant("zeros", dtype=tf.string))
+                              , tf.math.equal(padding_mode, tf.constant("min", dtype=tf.string))):
+            valid = tf.less_equal(ix, width - 1.) & tf.greater_equal(ix, zero) \
+                    & tf.less_equal(iy, height - 1.) & tf.greater_equal(iy, zero) \
+                    & tf.less_equal(iz, depth - 1.) & tf.greater_equal(iz, zero)
+            valid = tf.expand_dims(tf.cast(valid, dtype=tf.float32), -1)
 
-        # if using bilinear interpolation, calculate each area between corners and positions to get the weights for each input pixel
-        def bilinear():
-            output = tf.zeros(shape_output, dtype=tf.float32)
-            
+        # for bilinear interpolation, calculate each area between corners and positions to get each pixel's weight
+        if tf.math.equal(method, tf.constant("bilinear", dtype=tf.string)):
             # get north-west-top corner indexes based on the scaled positions
             ix_nwt = tf.clip_by_value(tf.floor(ix), zero, width - 1.)
             iy_nwt = tf.clip_by_value(tf.floor(iy), zero, height - 1.)
@@ -211,15 +253,12 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
                 ix_c = ix_nwt + offset_corner[-c - 1, 0]
                 iy_c = iy_nwt + offset_corner[-c - 1, 1]
                 iz_c = iz_nwt + offset_corner[-c - 1, 2]
-
                 # area is computed using the opposite corner
                 nc = tf.expand_dims(tf.abs((ix - ix_c) * (iy - iy_c) * (iz - iz_c)), -1)
-
                 # current corner position
                 ix_c = ix_nwt_i + offset_corner_i[c, 0]
                 iy_c = iy_nwt_i + offset_corner_i[c, 1]
                 iz_c = iz_nwt_i + offset_corner_i[c, 2]
-
                 # gather input image values from corners idx, and calculate weighted pixel value
                 idx_c = ibatch + tf.clip_by_value(ix_c, zero_i, width_i - 1) \
                         + num_row_major[0] * tf.clip_by_value(iy_c, zero_i, height_i - 1) \
@@ -227,9 +266,8 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
                 Ic = tf.gather(tf.reshape(im, [-1, channels]), idx_c)
 
                 output += nc * Ic
-            return output
-        # else if using nearest neighbor, get the nearest corner
-        def nearest_neighbor():
+        # otherwise for nearest neighbor, just get the nearest corner
+        elif tf.math.equal(method, tf.constant("nn", dtype=tf.string)):
             # get rounded indice corner based on the scaled positions
             ix_nn = tf.cast(tf.clip_by_value(tf.round(ix), zero, width - 1.), dtype=tf.int32)
             iy_nn = tf.cast(tf.clip_by_value(tf.round(iy), zero, height - 1.), dtype=tf.int32)
@@ -238,19 +276,13 @@ class SpatialTransformer3D(tf.keras.layers.Layer):
             # gather input pixel values from nn corner indexes
             idx_nn = ibatch + ix_nn + num_row_major[0] * iy_nn + num_row_major[1] * iz_nn
             output = tf.gather(tf.reshape(im, [-1, channels]), idx_nn)
-            return output
-
-        cond_bilinear = tf.math.equal(method, tf.constant("bilinear", dtype=tf.string))
-        cond_nn = tf.math.equal(method, tf.constant("nn", dtype=tf.string))
-        output = tf.case([(cond_bilinear, bilinear), (cond_nn, nearest_neighbor)], exclusive=True)
         
         # padding mode
-        cond_border = tf.math.equal(padding_mode, tf.constant("border", dtype=tf.string))
-        cond_zero = tf.math.equal(padding_mode, tf.constant("zeros", dtype=tf.string))
-        cond_value = tf.math.equal(padding_mode, tf.constant("min", dtype=tf.string))
-        def border_padding_mode(): return output
-        def zero_padding_mode(): return output * valid
-        def min_padding_mode(): return output * valid + tf.reduce_min(im) * (1. - valid)
-        output = tf.case([(cond_border, border_padding_mode), (cond_zero, zero_padding_mode), (cond_value, min_padding_mode)], exclusive=True)
+        if tf.math.equal(padding_mode, tf.constant("zeros", dtype=tf.string)):
+            output = output * valid
+        elif tf.math.equal(padding_mode, tf.constant("min", dtype=tf.string)):
+            output = output * valid + tf.reduce_min(im) * (1. - valid)
+        elif tf.math.equal(padding_mode, tf.constant("border", dtype=tf.string)):
+            output = output
 
         return output
--- tests/spatial_transformer_3D_test.py
+++ tests/spatial_transformer_3D_test.py
@@ -8,81 +8,87 @@ def test_spatial_transformer_3D_train():
     """Test a simple training loop."""
 
     seed = 0
-    params_spatial_transformer = dict(
-        min_ref_grid=[-1., -1., -1.],
-        max_ref_grid=[1., 1., 1.],
-        interp_method="bilinear",
-        padding_mode="min")
+    params_spatial_transformer = dict(interp_method="bilinear", padding_mode="border")
     volume_shape = tf.TensorShape((1, 10, 10, 10, 1))
     # source and target volumes are 3D-tensors of shape [A0, W, H, D, C]
     source = tf.random.normal(shape=volume_shape, dtype=tf.float32, seed=seed)
-    target = source + 1e-6
+    target = source + tf.random.normal(shape=volume_shape, stddev=1e-3, dtype=tf.float32, seed=seed)
     # train with 5 iterations
     num_iters = 5
+    # initialize weights with identity transformation slightly pertubed
+    init_quaternion = tf.constant([0., 0., 0., 1., 0., 0., 0., 1., 1., 1.])
+    init_quaternion = init_quaternion + tf.random.normal(shape=(10,), stddev=1e-6, dtype=tf.float32, seed=seed)
+    init_weights_quaternion = [tf.zeros((5, 10), dtype=tf.float32), init_quaternion]
 
     if tf.executing_eagerly():
-        # inputs
+        # input and model definition
         inp_source = tf.keras.Input(shape=volume_shape[1:], dtype="float32")
-        inp_target = tf.keras.Input(shape=volume_shape[1:], dtype="float32")
-        # model definition
-        summed = tf.keras.layers.Add()([inp_source, inp_target])
-        reshaped = tf.keras.layers.Flatten()(summed)
+        reshaped = tf.keras.layers.Flatten()(inp_source)
         reshaped = tf.expand_dims(reshaped, axis=-1)
         max_pooled = tf.keras.layers.MaxPool1D(pool_size=2, strides=200, padding="SAME")(reshaped)
         flattened = tf.keras.layers.Flatten()(max_pooled)
-        transformation = tf.keras.layers.Dense(units=7, activation=None)(flattened)
+        transformation = tf.keras.layers.Dense(units=10, activation=None, weights=init_weights_quaternion)(flattened)
         output = SpatialTransformer3D(**params_spatial_transformer)([inp_source, transformation])
-        model = tf.keras.models.Model(inputs=[inp_target, inp_source], outputs=[output])
+        model = tf.keras.models.Model(inputs=[inp_source], outputs=[output])
         # optimizer function
         optimizer = tf.keras.optimizers.SGD(learning_rate=1e-6)
         # training loop
         for _ in range(num_iters):
             with tf.GradientTape() as tape:
-                loss = tf.nn.l2_loss(model([source, target]) - target)
+                loss = tf.nn.l2_loss(model([source]) - target)
             grads = tape.gradient(loss, model.trainable_variables)
             optimizer.apply_gradients(zip(grads, model.trainable_variables))
             # Check that gradients has correct shape
             tf.debugging.assert_equal(len(grads), 2)
-            tf.debugging.assert_equal(grads[0].shape, tf.TensorShape((5, 7)))
-            tf.debugging.assert_equal(grads[1].shape, tf.TensorShape((7)))
+            tf.debugging.assert_equal(grads[0].shape, tf.TensorShape((5, 10)))
+            tf.debugging.assert_equal(grads[1].shape, tf.TensorShape((10)))
 
-def test_spatial_transformer_3D_forward_bilinear():
-    """Test a simple forward pass with bilinear resampling."""
+def test_spatial_transformer_3D_forward_methods():
+    """Test a simple forward pass with different interpolation and padding methods."""
 
-    params_spatial_transformer = dict(
-            min_ref_grid=[-1., -1., -1.],
-            max_ref_grid=[1., 1., 1.],
-            interp_method="bilinear",
-            padding_mode="min")
+    seed = 0
+    params_spatial_transformer = [
+        dict(interp_method="bilinear", padding_mode="border")
+        , dict(interp_method="nn", padding_mode="border")
+        , dict(interp_method="nn", padding_mode="zeros")
+        , dict(interp_method="nn", padding_mode="min")
+    ]
     volume_shape = (1, 10, 10, 10, 1)
     # source volume is 3D-tensors of shape [A0, W, H, D, C]
-    source = tf.random.normal(shape=volume_shape, dtype=tf.float32, seed=0)
-    # resample with identity quaternion and no translation
-    quaternion = tf.constant([[1., 0., 0., 0., 0., 0., 0.]], dtype=tf.float32)
+    source = tf.random.normal(shape=volume_shape, dtype=tf.float32, seed=seed)
+    # resample with identity transformation slightly pertubed
+    quaternion = tf.constant([[0., 0., 0., 1., 0., 0., 0., 1., 1., 1.]], dtype=tf.float32)
+    quaternion = quaternion + tf.random.normal(shape=(1, 10), stddev=1e-9, dtype=tf.float32, seed=seed)
 
     if tf.executing_eagerly():
-        output = SpatialTransformer3D(**params_spatial_transformer, trainable=False)([source, quaternion])
-        tf.debugging.assert_near(source, output)
+        for param_spatial_transformer in params_spatial_transformer:
+            output = SpatialTransformer3D(**param_spatial_transformer, trainable=False)([source, quaternion])
+            tf.debugging.assert_near(source, output, atol=1e-6, message="{}".format(param_spatial_transformer))
 
-def test_spatial_transformer_3D_forward_nn():
-    """Test a simple forward pass with nn resampling."""
+def test_spatial_transformer_3D_forward_transformations():
+    """Test a simple forward pass with different transformations."""
 
-    params_spatial_transformer = dict(
-            min_ref_grid=[-1., -1., -1.],
-            max_ref_grid=[1., 1., 1.],
-            interp_method="nn",
-            padding_mode="min")
+    seed = 0
+    params_spatial_transformer = dict(interp_method="bilinear", padding_mode="border")
     volume_shape = (1, 10, 10, 10, 1)
     # source volume is 3D-tensors of shape [A0, W, H, D, C]
-    source = tf.random.normal(shape=volume_shape, dtype=tf.float32, seed=0)
-    # resample with identity quaternion and no translation
-    quaternion = tf.constant([[1., 0., 0., 0., 0., 0., 0.]], dtype=tf.float32)
+    source = tf.random.normal(shape=volume_shape, dtype=tf.float32, seed=seed)
+    # resample with identity transformation slightly pertubed
+    quaternions = [
+        tf.constant([[0., 0., 0., 1., 0., 0., 0., 1., 1., 1.]], dtype=tf.float32) \
+            + tf.random.normal(shape=(1, 10), stddev=1e-6, dtype=tf.float32, seed=seed)
+        , tf.constant([[0., 0., 0., 1., 0., 0., 0.]], dtype=tf.float32) \
+            + tf.random.normal(shape=(1, 7), stddev=1e-6, dtype=tf.float32, seed=seed)
+        , tf.constant([[0., 0., 0., 1.]], dtype=tf.float32) \
+            + tf.random.normal(shape=(1, 4), stddev=1e-6, dtype=tf.float32, seed=seed)
+    ]
 
     if tf.executing_eagerly():
-        output = SpatialTransformer3D(**params_spatial_transformer, trainable=False)([source, quaternion])
-        tf.debugging.assert_near(source, output)
+        for quaternion in quaternions:
+            output = SpatialTransformer3D(**params_spatial_transformer, trainable=False)([source, quaternion])
+            tf.debugging.assert_near(source, output, atol=1e-3, message="{}".format(quaternion))
 
 if __name__ == "__main__":
     test_spatial_transformer_3D_train()
-    test_spatial_transformer_3D_forward_bilinear()
-    test_spatial_transformer_3D_forward_nn()
+    test_spatial_transformer_3D_forward_methods()
+    test_spatial_transformer_3D_forward_transformations()
